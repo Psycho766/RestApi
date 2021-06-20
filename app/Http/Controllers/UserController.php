f8a93c45986a902ae3fe78f7475eeaaec4ef55c2
Psycho766 <brianmacatangay2@gmail.com>
2021-06-21 06:29:01 +0800

    Usercontroller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,10 +1,10 @@
 <?php
 namespace App\Http\Controllers;
 use Illuminate\Http\Request;
-
+//Use User Model
 use App\User;
 //Use Resources to convert into json
-use app\Http\Resources\UserResource as UserResource;
+use App\Http\Resources\UserResource as UserResource;
 class UserController extends Controller
 {
     public function getusers()
